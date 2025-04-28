f5d315eb52549dbbf2d1f68c45e1eb8a29ddfc18
Sriman K <sriman.k@pravaltech.com>
2025-04-28 17:14:21 +0530

    a min stable commit of chat bot

--- src/extensions/components/ChatBox.tsx
+++ src/extensions/components/ChatBox.tsx
@@ -1,49 +1,140 @@
-import * as React from 'react';
-import styles from './ChatBox.module.scss';
-// import styles from './ChatBox.module.scss';
 
-interface IChatBoxProps { 
-  user: any;
+import * as React from 'react';
+import styles from './ChatBox.module.scss'; 
+
+interface IChatBoxProps {
+  user: {
+    loginName: any; displayName: string;  
+};
 }
- 
+
+interface IMessage {
+  sender: 'user' | 'bot';
+  content: string;
+  timestamp: string;
+}
+
 interface State {
-  IsChatActive:boolean;
-  
+  isChatActive: boolean;
+  inputText: string;
+  messages: IMessage[];
 }
 
 class ChatBox extends React.Component<IChatBoxProps, State> {
+  private storageKey = 'spfx-chat-messages';
+ userImage = `/_layouts/15/userphoto.aspx?size=S&accountname=${this.props.user.loginName}`;
   constructor(props: IChatBoxProps) {
     super(props);
-    this.state = { 
-      IsChatActive:false,
-      };
-    this.handleChatClick = this.handleChatClick.bind(this);
+    this.state = {
+      isChatActive: false,
+      inputText: '',
+      messages: [],
+    };
   }
-  
-  public handleChatClick(){
-    console.log("clicked the chat buttom !!!"+this.props.user.displayName);
-    this.setState({
-      IsChatActive: !this.state.IsChatActive,
-    })
-  }
-  render() { 
-    return (
-      <div>  
-      <div className={styles.chatButton} onClick={this.handleChatClick}> Chat here 💬</div>
-     {this.state.IsChatActive  && 
-       <div className={styles.chatBox}>
-        <div className={styles.ChatBoxheader}>
-            <div className={styles.title}>Your sharepoint assistant</div>
-            <div className={styles.close}>close</div>
-        </div>
-
-      </div>
-      
+  componentDidMount() {
+    const persisted = localStorage.getItem(this.storageKey);
+    if (persisted) {
+      this.setState({ messages: JSON.parse(persisted) });
     }
-      </div>
+    
+  }
+
+  componentDidUpdate(_: IChatBoxProps, prevState: State) {
+    if (prevState.messages !== this.state.messages) {
+      localStorage.setItem(this.storageKey, JSON.stringify(this.state.messages));
+    }
+  }
+
+  handleChatToggle = () => {
+    this.setState(
+      prev => ({ isChatActive: !prev.isChatActive }),
+      () => {
+        if (this.state.isChatActive && this.state.messages.length === 0) {
+          this.addBotMessage(`Hello ${this.props.user.displayName}, how can I help you?`);
+        }
+      }
+    );
+  };
+
+  handleClose = () => {
+    this.setState({ isChatActive: false });
+  };
+
+  handleMsgSend = () => {
+    const text = this.state.inputText.trim();
+    if (text) {
+      this.addMessage('user', text);
+      this.setState({ inputText: '' });
+      setTimeout(() => this.addBotMessage('I am here to assist you.'), 500);
+    }
+  };
+
+  handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ inputText: e.target.value });
+  };
+
+  private addMessage(sender: 'user' | 'bot', content: string) {
+    const timestamp = new Date().toLocaleTimeString(undefined, { hour: '2-digit', minute: '2-digit' });
+    this.setState(prev => ({ messages: [...prev.messages, { sender, content, timestamp }] }));
+  }
+
+  private addBotMessage(msg: string) {
+    this.addMessage('bot', msg);
+  }
+
+  render() {
+    // const { user } = this.props;
+    return (
+      <>
+        <div className={styles.chatButton} onClick={this.handleChatToggle}>
+          Chat here 💬
+        </div>
+        {this.state.isChatActive && (
+          <div className={styles.chatBox}>
+            <div className={styles.header}>
+              <div className={styles.title}>Your SharePoint Assistant</div>
+              <button className={styles.close} onClick={this.handleClose}>×</button>
+            </div>
+
+            <div className={styles.chatsContainer}>
+              {this.state.messages.map((m, i) => (
+                <div
+                  key={i}
+                  className={`${styles.chatMessage} ${
+                    m.sender === 'user' ? styles.userMessage : styles.botMessage
+                  }`}
+                >
+                  <div className={styles.profilePic}>
+                    <img
+                      src={
+                        m.sender === 'user' ? this.userImage : "https://cdn-icons-png.flaticon.com/512/18355/18355220.png"
+                      }
+                      alt={m.sender}
+                    />
+                  </div>
+                  <div className={styles.bubble}>
+                    <p>{m.content}</p>
+                    <span className={styles.timestamp}>{m.timestamp}</span>
+                  </div>
+                </div>
+              ))}
+            </div>
+
+            <div className={styles.inputRow}>
+              <input
+                type="text"
+                value={this.state.inputText}
+                onChange={this.handleInputChange}
+                onKeyPress={e => e.key === 'Enter' && this.handleMsgSend()}
+                placeholder="Start typing..."
+              />
+              <button onClick={this.handleMsgSend}>Send</button>
+            </div>
+          </div>
+        )}
+      </>
     );
   }
 }
- 
-export default ChatBox;
 
+export default ChatBox;
